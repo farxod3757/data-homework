0578fbde8b6ea2eb2f2c00ef375c48f7158e7fd0
Farxod <farxodyangiboyev3@gmail.com>
2026-03-06 23:03:13 +0500

    hw-22 farxod tamonidan qilindi

---
+++ hw-22.py
@@ -0,0 +1,37 @@
+# 1.Berilgan gapdagi barcha raqamlarni olib tashlang, faqat harflar qolsin.
+# o=""
+# v="0123456789"
+# x="men 17 yoshdaman va 2008 chi yilda tug'ilganman"
+# for l in x:
+#     ch=0
+#     for u in v:
+#         if l==u:
+#            ch=ch+1
+#     if ch==0: 
+#        o=o+l
+# print(o)
+
+# 2.Foydalanuvchi kiritgan so‘z palindrom ekanligini tekshiring.
+# ol=""
+# soz=input("soz: ")
+# soz=soz.replace(" ","")
+# for x in soz:
+#     ol=x+ol
+# if ol==soz:
+#     print("bu so'z palidrom soz")
+# else:
+#     print("palidrom emas")
+
+    
+
+# 4. So‘zlar ichidagi unli harflarni “*” belgisi bilan almashtiring.
+# x="men 17 yo'shdaman va 2008 chi yilda tug'ilganman"
+# v="euoai"
+# b=""
+# for m in x:
+#     for s in v:
+#         if m==s:
+#             m="*"
+#     b=b+m
+# print(b)
+
